4d328c0568783a87e5ba56b1200f0beda1d35bc6
Torben Jepsen <torbenjepsen@outlook.com>
2018-05-15 14:57:32 -0500

    finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,9 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      user: '',
-      city: ''
-    };
+    this.state = {user: {name: '', city: ''}} ;
+    
+
    
 
     // makes 'this' in handlechange the same as this in the constructor
@@ -18,23 +17,23 @@ class App extends Component {
     // console.log('input was changed');
     console.log('event.target', event.target.value)
     // this.state.user = event.target.value;
-    this.setState({ user: event.target.value });
+    this.setState({user: { name: event.target.value, city: this.state.user.city }});
   }
 
   handleCityChange = (event) => {
-    this.setState({ city: event.target.value });
+    this.setState({user: { city: event.target.value, name: this.state.user.name}});
   }
 
   buttonClick = () => {
-    console.log(this.state);
+    console.log(this.state.user);
   }
 
   render() {
     return (
       <div>
-        <p>User {this.state.user}</p>
+        <p>User {this.state.user.name}</p>
         <input onChange={this.handleChange} placeholder="username" />
-        <p>is from {this.state.city}</p>
+        <p>is from {this.state.user.city}</p>
         <input onChange={this.handleCityChange} placeholder="city" />
         <button onClick={this.buttonClick}> Submit </button>
       </div>
